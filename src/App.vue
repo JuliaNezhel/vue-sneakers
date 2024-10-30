3218cfa9d11e63e4126263084139170a6f318442
yu.nezhelskaya <yu.nezhelskaya@qpdev.ru>
2024-10-30 14:49:10 +0300

    подключени тайл винд и добавление иконок

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<template><div>hello</div></template>
+<template>
+  <div>hello</div>
+</template>
 
+<!-- setup это показывает что будет использовать composition APi -->
 <script setup lang="ts"></script>
 
+<!-- scope это инкапсуляция стилей своего рода модули  -->
 <style scoped></style>
